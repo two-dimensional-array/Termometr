017517c4039182f3751982e914fd0b93c6787063
two-dimensional-array <andreykolomiets630@gmail.com>
2026-01-10 14:11:51 +0200

    Fix connect to WiFi retry.

--- components/WiFi/wifi_sta.c
+++ components/WiFi/wifi_sta.c
@@ -19,6 +19,7 @@ typedef enum
 #define WIFI_EVENTS_BIT_MASK (WIFI_CONNECTED_BIT | WIFI_FAIL_BIT)
 
 static EventGroupHandle_t sta_event_group = NULL;
+static esp_netif_t* sta_netif = NULL;
 
 static const char *TAG = "wifi_sta";
 
@@ -45,7 +46,10 @@ esp_err_t wifi_connect_sta(const char *ssid, const char *password, uint32_t time
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &wifi_sta_event_handler, NULL, &wifi_event_instance));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL, &ip_event_instance));
 
-    esp_netif_create_default_wifi_sta();
+    if (sta_netif == NULL)
+    {
+        sta_netif = esp_netif_create_default_wifi_sta();
+    }
 
     wifi_config_t wifi_config = { 0 };
     strncpy((char*)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid)-1);
